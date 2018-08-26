980be80490a367614c46055eadb27386316124dc
Luca Merli <luca.merli84@gmail.com>
2018-08-26 16:58:30 +0200

    Delete mysqli.inc.php

--- includes/mysqli.inc.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Procedure di connessione
-$host="salvassicidb.mysql.db";
-$user="salvassicidb";
-$pass="vX5VMfEu7P";
-$database="salvassicidb";
-
-$mysqli = new mysqli($host, $user, $pass, $database);
-?>
